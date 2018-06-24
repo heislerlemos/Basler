75de357e5d47172f134d436f5865cef47c5c5f76
heisler lemos <heisler.lemos@gmail.com>
2018-06-24 10:11:48 +0100

    added client page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( bemvindos.css )
-
+Rails.application.config.assets.precompile += %w( clients.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
