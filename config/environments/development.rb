7678bb324abc4ecf65806248e9cd218bb4543107
heislerlemos <heisler.lemos@ita.co.ao>
2018-07-13 20:09:56 +0100

    styling the mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,18 +52,20 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
 
 config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+config.action_mailer.perform_deliveries = true
 config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
-address: “smtp.gmail.com”,
+address: "smtp.gmail.com",
 port: 587,
-domain: ENV[“gmail.com”],
-authentication: “plain”,
+domain: ENV["gmail.com"],
+authentication: "plain",
 enable_starttls_auto: true,
-user_name: ENV[“lamportruby@gmail.com”],
-password: ENV[“lamport1234567890”]
+user_name: ENV["lamportruby@gmail.com"] ,
+password: ENV["lamport1234567890"]
 }
+
 end
 
