5682d3f7527ebc2a3d10bd517ee661eaa96d083c
heislerlemos <heisler.lemos@ita.co.ao>
2018-07-17 08:54:11 +0100

    getting around on mailform gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,13 +58,13 @@ config.action_mailer.perform_deliveries = true
 config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
-address: "smtp.gmail.com",
-port: 587,
-domain: ENV["gmail.com"],
-authentication: "plain",
-enable_starttls_auto: true,
-user_name: ENV["lamportruby@gmail.com"] ,
-password: ENV["lamport1234567890"]
+   address:              'smtp.gmail.com',
+   port:                 587,
+   domain:               'gmail.com',
+   user_name:            'lamportruby',
+   password:             'lamport1234567890',
+   authentication:       'plain',
+   enable_starttls_auto: true  
 }
 
 end
