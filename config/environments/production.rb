b3a7d6308e32e84f62dd688f6d7e08494ec41180
heisler lemos <heisler.lemos@gmail.com>
2018-08-22 19:36:23 +0100

    testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'basler.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 config.action_mailer.perform_deliveries = true
 config.action_mailer.delivery_method = :smtp
 
